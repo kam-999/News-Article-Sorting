01eb8bc18867ac36e2c530127b9d0769dc0dc08c
Kamal <47293475+kam-999@users.noreply.github.com>
2024-12-08 00:19:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.responses import HTMLResponse, RedirectResponse
+from starlette.responses import HTMLResponse
 from uvicorn import run as app_run
 
 from typing import Optional
+import os
 
 from BBC_News.constants import APP_HOST, APP_PORT
 from BBC_News.pipeline.prediction_pipeline import BBCNewsData, BBCNewsClassifier
@@ -28,6 +29,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+log_dir = 'logs'
+
 class ArticleForm:
     def __init__(self, request: Request):
         self.request: Request = request
@@ -39,33 +42,30 @@ class ArticleForm:
 
 @app.get("/", tags=["authentication"])
 async def index(request: Request):
-
     return templates.TemplateResponse(
-            "BBCNews.html", {"request": request, "context": "Rendering"})
+        "BBCNews.html", {"request": request, "context": "Rendering", "logs": ""}
+    )
 
 @app.get("/train")
 async def trainRouteClient():
     try:
         train_pipeline = TrainPipeline()
-
         train_pipeline.run_pipeline()
 
         return Response("Training successful !!")
-
     except Exception as e:
         return Response(f"Error Occurred! {e}")
-
 
 @app.post("/")
 async def predictRouteClient(request: Request):
     try:
         form = ArticleForm(request)
         await form.get_article_data()
-        
+
         text_data = BBCNewsData(
             text=form.text_article
         )
-        
+
         text_df = text_data.get_bbc_input_data_frame()
 
         model_predictor = BBCNewsClassifier()
@@ -83,13 +83,37 @@ async def predictRouteClient(request: Request):
 
         status = category_mapping.get(value, "Unknown")
 
+        # Get latest log content
+        log_file = max(
+            (os.path.join(log_dir, f) for f in os.listdir(log_dir)), 
+            key=os.path.getctime
+        )
+        with open(log_file, 'r') as file:
+            logs = file.read()
+
         return templates.TemplateResponse(
             "BBCNews.html",
-            {"request": request, "context": status},
+            {"request": request, "context": status, "logs": logs},
         )
         
     except Exception as e:
         return {"status": False, "error": f"{e}"}
+
+@app.get("/logs", tags=["logs"])
+async def get_logs():
+    try:
+        log_file = max(
+            (os.path.join(log_dir, f) for f in os.listdir(log_dir)), 
+            key=os.path.getctime
+        )  # Get the latest log file
+
+        with open(log_file, 'r') as file:
+            log_content = file.read()
+
+        return {"status": True, "logs": log_content}
+
+    except Exception as e:
+        return {"status": False, "error": str(e)}
 
 
 if __name__ == "__main__":
