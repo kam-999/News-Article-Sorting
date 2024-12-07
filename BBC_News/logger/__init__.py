a0ca69fdfdaf8f6e895758f797ab600489553c2a
Kamal <47293475+kam-999@users.noreply.github.com>
2024-12-08 00:20:59 +0530

    Update logger

--- BBC_News/logger/__init__.py
+++ BBC_News/logger/__init__.py
@@ -18,3 +18,6 @@ logging.basicConfig(
     format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
     level=logging.DEBUG,
 )
+
+logging.getLogger("pymongo").setLevel(logging.ERROR)
+logging.getLogger("botocore").setLevel(logging.ERROR)
